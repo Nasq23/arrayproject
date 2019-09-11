81f9eecd73cb1e67f5b4498373365ad1eab3b9a1
GBTC441009UR <GBTC441009UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-11 14:02:31 -0400

    array project

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String args[]){
+
+
+int[] true1 = new int[10];
+Scanner input = new Scanner (System.in);
+
+for(int k10=0; k10 <10; k10++){
+
+
+
+        System.out.println("Please enter your next number "+ (k10+1) + ":");
+
+    }
+
+
+
+for(int k10=0; k10 <10; k10++){
+
+
+}
+}
+}
